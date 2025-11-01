68ad0ea22f1c08b6db004ddaca0b89432f47d81f
HarpLoid <harploidsoft@gmail.com>
2025-11-01 13:40:48 +0100

    feat(properties): Implement caching for property retrieval in views

---
+++ properties/utils.py
@@ -0,0 +1,14 @@
+from django.core.cache import cache
+from .models import Property
+
+def get_all_properties():
+    
+    properties = cache.get('all_properties')
+    
+    if not properties:
+        properties = list(Property.objects.all().values(
+            "property_id", "title", "description", "price", "location", "created_at"
+        ))
+        cache.set('all_properties', properties, 3600)
+    
+    return properties
--- properties/views.py
+++ properties/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.cache import cache_page
+from .utils import get_all_properties
 from .models import Property
 
 @cache_page(60 * 15)
 def property_list(request):
-    properties = Property.objects.all().values(
-        "property_id", "title", "description", "price", "location", "created_at"
-    )
+    properties = get_all_properties()
     return JsonResponse({"data": list(properties)}, safe=False)
